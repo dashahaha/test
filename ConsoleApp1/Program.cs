d21129d5f6c857e186693a657892b94926390b56
dashahaha <45868697+dashahaha@users.noreply.github.com>
2018-12-14 11:47:10 +0300

    new2
    2.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!!!");
-            Console.WriteLine("Hello world2");
+            Console.WriteLine("Hello world2.1");
         }
     }
 }
